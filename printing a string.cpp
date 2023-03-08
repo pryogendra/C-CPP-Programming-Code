47d3f322f7903bdcfd5d768899544bcb526df717
Yogendra Prasad <127005415+pryogendra@users.noreply.github.com>
2023-03-08 23:18:53 +0530

    Add files via upload

---
+++ printing a string.cpp
@@ -0,0 +1,7 @@
+#include<iostream>          //include header file
+using namespace std;
+int main()
+{
+	cout<<"C++ is better than c language. \n";      //c++ statement
+	return 0;
+}                     //end of program
