9922c75488176212ec6210694eac80d9396b005a
Yogendra Prasad <127005415+pryogendra@users.noreply.github.com>
2023-03-09 22:58:48 +0530

    Add files via upload
    Signed-off-by: Yogendra Prasad <127005415+pryogendra@users.noreply.github.com>

---
+++ Fahrenheit to Celsius.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float fahrenheit,celsius;
+	cout<<"Enter temperature in Fahrenheit : ";
+	cin>>fahrenheit;
+	celsius=(fahrenheit-32)*5/9;
+	cout<<fahrenheit<<" F = "<<celsius<<" C";
+}
