977a9841694a5aa27356fb74839002b03ad26190
Yogendra Prasad <127005415+pryogendra@users.noreply.github.com>
2023-03-09 22:47:02 +0530

    Add files via upload
    Signed-off-by: Yogendra Prasad <127005415+pryogendra@users.noreply.github.com>

---
+++ ASCII value.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	char str;
+	cout<<"Enter a charecter : ";
+	cin>>str;
+	int ASCII=(int)str;
+	cout<<"ASCII value of the entered charecter is "<<ASCII;
+}
